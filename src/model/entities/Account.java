50443479a3e02d70ee3882cbff32eb56314c6883
jonasmachados <jonasmachado.ti@gmail.com>
2022-04-25 20:08:56 -0300

    Created class Account

---
+++ src/model/entities/Account.java
@@ -0,0 +1,73 @@
+package model.entities;
+
+public class Account {
+
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+
+    //CONSTRUCTOR
+    public Account() {
+    }
+
+    //CONSTRUCTOR
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+    
+    //TOSTRING
+    @Override
+    public String toString() {
+        return "Account: " 
+                + "number: " 
+                + number 
+                + ", holder: " 
+                + holder
+                + ", balance: " 
+                + balance
+                + ", withdrawLimit: " + withdrawLimit + '}';
+    }
+    
+    public void deposit(Double amount){
+        
+    }
+    
+    public void withdrawLimit(Double amount){
+        
+    }
+    
+    //GET AND SET
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void setWithdrawLimit(Double withdrawLimit) {
+        this.withdrawLimit = withdrawLimit;
+    }
+  
+
+}
